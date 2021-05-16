a2cc4bbf974b8f4f222d6adbc665acf44d1dad28
Atlas Fatima Anahi <atlasf@tasa.telefonica.com.ar>
2021-05-15 23:28:33 -0300

    Repasando clase 8

---
+++ variables.js
@@ -0,0 +1,30 @@
+
+
+
+var Nombre = "Anahi"; //Asigno una variable//
+
+console.log(Nombre);  //Escribir la variable por la consola//
+
+var numero = 1000
+console.log(numero);
+
+var persona = {
+    Nombre: "Nico",
+    Apellido: "Fernandez",
+    telefono: 1112345678,
+};
+
+console.log(persona)
+
+
+//Trabajando con el DOM
+let titulo = document.getElementById ('titulo'); //Obtengo el elemento de HTML//
+
+//Escribo en el archivo HTML//
+titulo.innerHTML = "Nuevo titulo para el index de HTML";
+
+
+let parrafo = document.getElementById('parrafo'); //Obtengo el elemento en HTML//
+
+//Escribo en un archivo HTML//
+parrafo.innerHTML = "Nuevo parrafo para el index de HTML";
